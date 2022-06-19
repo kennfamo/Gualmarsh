63a42067867afafb93e7ff7807907f006fd9cfec
josgabfer <josgabfer@outlook.com>
2022-06-19 11:37:02 -0600

    Added Model, DBContext, Packages and BackEnd project

---
+++ BackEnd/Model/Cantons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Model
+{
+    internal class Cantons
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [ForeignKey("Provinces")]
+        public int ProvincesId { get; set; }
+    }
+}
---
+++ BackEnd/Model/Cities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Model
+{
+    internal class Cities
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [ForeignKey("Cantons")]
+        public string CantonId { get; set; }
+    }
+}
