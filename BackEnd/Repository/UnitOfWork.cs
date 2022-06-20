8319b742baec8930c3577fad7f8d986bf1001ada
Daniel Coto <dcoto30607@ufide.ac.cr>
2022-06-20 13:50:28 -0600

    UnitOfWork and Repository implemented

---
+++ BackEnd/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using BackEnd.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.Repository
+{
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
