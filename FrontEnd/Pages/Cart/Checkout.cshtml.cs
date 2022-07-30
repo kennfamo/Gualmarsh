8fd910f8410bed3c1d99691c0b7b0aaea81de836
Daniel Coto <dcoto30607@ufide.ac.cr>
2022-07-29 18:48:22 -0600

    Checkout still on work

--- FrontEnd/Pages/Cart/Checkout.cshtml.cs
+++ FrontEnd/Pages/Cart/Checkout.cshtml.cs
@@ -3,6 +3,7 @@ using BackEnd.Repository.IRepository;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Security.Claims;
 
 namespace FrontEnd.Pages.Cart
@@ -12,6 +13,9 @@ namespace FrontEnd.Pages.Cart
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
         public IEnumerable<UserAddress> UserAddressList { get; set; }
+        public IEnumerable<SelectListItem> CityList { get; set; }
+        public IEnumerable<SelectListItem> ProvinceList { get; set; }
+        public IEnumerable<SelectListItem> CantonList { get; set; }
         public OrderHeader OrderHeader { get; set; }
         public UserAddress UserAddress { get; set; }
         private readonly IUnitOfWork _unitOfWork;
@@ -37,7 +41,29 @@ namespace FrontEnd.Pages.Cart
                 ApplicationUser applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value);
                 UserAddressList  = _unitOfWork.UserAddress.GetAll(filter: u => u.ApplicationUserId == claim.Value,
                     includeProperties: "City,City.Canton,City.Canton.Province,ApplicationUser");
+                ProvinceList = _unitOfWork.Province.GetAll().Select(i => new SelectListItem()
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });               
+
+                CityList = _unitOfWork.City.GetAll().Select(i => new SelectListItem()
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                });
             }
+        }
+
+        public JsonResult OnGetCanton([FromQuery] int param)
+        {
+            //int province = Int32.Parse(param);
+            CantonList = _unitOfWork.Canton.GetAll(filter: u => u.ProvinceId == param).Select(i => new SelectListItem()
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            return new JsonResult(CantonList);
         }
     }
 }
---
+++ FrontEnd/wwwroot/js/pages/checkout.js
@@ -0,0 +1,13 @@
+﻿function GetCanton() {
+    var param = $('#province').val();
+    $.ajax({
+        type: 'GET',
+        url: '../Cart/Checkout?handler=Canton?param=' + param,
+        dataType: 'json',
+        data: { 'ProvinceId': $('#province').val() },
+        cache: false,
+        success: function (data) {
+            alert(data);
+        },               
+    })
+};
