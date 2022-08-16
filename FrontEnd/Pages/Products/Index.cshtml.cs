fc4d56c437d90c34eb4bb0f8263e802085b0bfb8
josgabfer <josgabfer@outlook.com>
2022-08-15 22:54:25 -0600

    Paging working now :) my commits are special

--- FrontEnd/Pages/Products/Index.cshtml.cs
+++ FrontEnd/Pages/Products/Index.cshtml.cs
@@ -1,5 +1,6 @@
 using BackEnd.Model;
 using BackEnd.Repository.IRepository;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
@@ -10,13 +11,17 @@ namespace FrontEnd.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(IUnitOfWork unitOfWork)
+        public IndexModel(IUnitOfWork unitOfWork, IConfiguration configuration)
         {
             _unitOfWork = unitOfWork;
+            Configuration = configuration;
         }
 
+        public PaginatedList<Product> Products { get; set; }
         public IEnumerable<Product> ProductList { get; set; }
+        public IEnumerable<Product> ProductListAll { get; set; }
         public IEnumerable<ProductSubcategory> ProductSubcategoryList { get; set; }
         public IEnumerable<ProductSubcategory> ProductSubcategoryListFilter { get; set; }
         public ProductSubcategory ProductSubcategory { get; set; }
@@ -24,13 +29,27 @@ namespace FrontEnd.Pages.Products
         public double FilteredMinPrice { get; set; }
         public double MaxPrice { get; set; }
         public double MinPrice { get; set; }
-        public void OnGet(string name, double min_price, double max_price)
+
+        [BindProperty(SupportsGet = true)]
+        public int PageIndex { get; set; } = 1;
+        public int Count { get; set; }
+        public int TotalPages { get; set; }
+        
+
+
+        public void OnGet(string name, double min_price, double max_price, int pageIndex)
         {
+
+
             ProductSubcategoryList = _unitOfWork.ProductSubcategory.GetAll(filter: u => u.Name == name);
             ProductSubcategoryListFilter = _unitOfWork.ProductSubcategory.GetAll(includeProperties: "ProductCategory");
             ProductSubcategory = _unitOfWork.ProductSubcategory.GetFirstOrDefault(filter: u => u.Name == name);
-            ProductList = _unitOfWork.Product.GetAll(filter: u => u.ProductSubcategoryId == ProductSubcategory.Id,
+            ProductListAll = _unitOfWork.Product.GetAll(filter: u => u.ProductSubcategoryId == ProductSubcategory.Id,
                     includeProperties: "ProductSubcategory,ProductSubcategory.ProductCategory");
+            ProductList = _unitOfWork.Product.GetAll(filter: u => u.ProductSubcategoryId == ProductSubcategory.Id,
+                    includeProperties: "ProductSubcategory,ProductSubcategory.ProductCategory").OrderBy(u => u.Id).Skip((pageIndex -1) * 6).Take(6);
+            Count = ProductListAll.Count();
+            TotalPages = (int)Math.Ceiling(decimal.Divide(Count, 6));
             foreach (var product in ProductList)
             {
                 if (product.Price > MaxPrice)
@@ -51,7 +70,10 @@ namespace FrontEnd.Pages.Products
                     includeProperties: "ProductSubcategory,ProductSubcategory.ProductCategory");
                 FilteredMinPrice = min_price;   
             }
-            
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            //Products = await PaginatedList<Product>.CreateAsync(
+            //   studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
+
         }
         
         
